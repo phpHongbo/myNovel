0147cfe520a930b8ea595e995d4f4397f6ec73f0
PHPcrr <983003865@qq,com>
2019-04-16 20:51:11 +0800

    初始版

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//后台路由组
+Route::group([],function(){
+	//后台首页
+	Route::get('/', function () {
+	    // return view('welcome');
+	    return view('admin/index');
+	});
+	//后台欢迎页
+	Route::get('/admin/welcome', function () {
+	    return view('admin/welcome');
+	});
 });
+
+
